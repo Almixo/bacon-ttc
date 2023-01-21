28b4a002cbc08a03fc4b41c9f9e32a014aec3123
Almixo <{ID}+{username}@users.noreply.github.com>
2023-01-21 14:42:44 +0100

    CDisplacer and CDispPoint.

--- sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp
+++ sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp
@@ -24,6 +24,10 @@ STUB_WEAPON_CLASS( weapon_alyxgun, WeaponAlyxGun, C_HLSelectFireMachineGun );
 STUB_WEAPON_CLASS( weapon_citizenpackage, WeaponCitizenPackage, C_BaseHLCombatWeapon );
 STUB_WEAPON_CLASS( weapon_citizensuitcase, WeaponCitizenSuitcase, C_WeaponCitizenPackage );
 
+#ifdef MAPBASE
+STUB_WEAPON_CLASS( weapon_displacer, Displacer, C_BaseHLCombatWeapon );
+#endif
+
 #ifndef HL2MP
 STUB_WEAPON_CLASS( weapon_ar2, WeaponAR2, C_HLMachineGun );
 STUB_WEAPON_CLASS( weapon_frag, WeaponFrag, C_BaseHLCombatWeapon );
